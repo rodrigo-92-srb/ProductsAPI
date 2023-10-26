a3406723b534bc30e662429969f23c04e700b9d7
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2023-10-26 15:52:38 -0300

    Add documentation with Swagger

--- src/main/java/br/com/api/products/ProductsApplication.java
+++ src/main/java/br/com/api/products/ProductsApplication.java
@@ -1,9 +1,12 @@
 package br.com.api.products;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Products API", version = "1.0", description = "Documentation about API Spring Boot"))
 public class ProductsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/api/products/controllers/ProductController.java
+++ src/main/java/br/com/api/products/controllers/ProductController.java
@@ -3,6 +3,8 @@ package br.com.api.products.controllers;
 import br.com.api.products.models.ProductModel;
 import br.com.api.products.models.ProductResponseModel;
 import br.com.api.products.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,32 +12,32 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/products")
+@Tag(name = "Product Controller", description = "Operations about products")
 public class ProductController {
 
     @Autowired
     private ProductService ps;
 
-    @GetMapping("/")
-    public String route(){
-        return "Products API working...";
-    }
-
     @GetMapping("/list")
+    @Operation(summary = "List all products", description = "Return a list of all products.")
     public Iterable<ProductModel> list(){
         return ps.list();
     }
 
     @PostMapping("/create")
+    @Operation(summary = "Create a new product", description = "Create a new product with request body informations")
     public ResponseEntity<?> create(@RequestBody ProductModel pm, String action){
         return ps.createOrUpdate(pm, "create");
     }
 
     @PutMapping("/update")
+    @Operation(summary = "Update a product", description = "Update a product with request body informations")
     public ResponseEntity<?> update(@RequestBody ProductModel pm, String action){
         return ps.createOrUpdate(pm, "update");
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete a product", description = "Delete a product by Id")
     public ResponseEntity<ProductResponseModel> delete(@PathVariable Long id){
         return ps.remove(id);
     }
